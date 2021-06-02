1c23b03aefa8040e43aca6878bc4d9d6a7668753
LFoz <luisceronl@outlook.com>
2021-06-02 01:48:01 -0500

    Final

--- Controls.js
+++ Controls.js
@@ -1,6 +1,5 @@
 //Move
 var MoveX=0,MoveY=0;
-var Shot=1;
 function Move(v){
     switch(v) {
       case 'MoveYUp':
@@ -37,6 +36,15 @@ function Move(v){
 //Auto Analysis function
 $(document).ready(function () {
   $("#AutoAnalysis").click(function () {
+    navigator.getMedia = (navigator.getUserMedia || 
+                          navigator.webkitGetUserMedia||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
+    navigator.getMedia({video:true}, function(){
+      console.log("Prueba");
+    }, function () {
+      console.log("No encendida");
+    });
     do{
       for(var i = 0; i <=12; i++){
         MoveX=MoveX+2         
@@ -125,13 +133,19 @@ $(document).ready(function () {
 });
 
 //Camera
-
 $(document).ready(function() {
   //Start Camera
   var Video = document.getElementById("Video"),
-  canvas = document.getElementById("LookPhoto"),
-  photo = document.getElementById('photo');
+      Video2 = document.getElementById("Video2"),
+      canvas = document.getElementById("LookPhoto"),
+      canvas2 = document.getElementById("LookPhoto2"),
+      photo = document.getElementById('photo');
   Video.unavalable = false;
+  Video2.unavalable =false;
+  canvas.width=canvas2.width=640;
+  canvas.height=canvas2.height=480;
+  var context = canvas.getContext('2d'),
+      context2 = canvas2.getContext('2d')
 
   $("#StartCamara").click(function () {
     if(Video.unavalable == false){
@@ -141,7 +155,9 @@ $(document).ready(function() {
                           navigator.msGetUserMedia);
     navigator.getMedia({video: true}, function (stream) { 
     Video.srcObject = stream; //permission granted
+    Video2.srcObject= stream;
     Video.play();
+    Video2.play();
     Video.unavalable = true;
     }, function () {
     alert("Camera Not Avaiable"); //permission not granted
@@ -151,16 +167,54 @@ $(document).ready(function() {
   }
   });
 
-  //Take Photo function "behavior"
+  //Take Photo behavior
   $("#TakePhoto").click(function () {
     if (Video.unavalable == true) {
       $("#CamOff").css("opacity", 0);  //Camera on
       $("#LookPhoto").css("opacity", 1);
-      $("#buttonbox").css("opacity", 1);      
-      var Phot;
-      Phot = document.getElementById("LookPhoto");
-      var context = Phot.getContext('2d');
-      context.drawImage(Video, 0, 0, 640, 480);
+      $("#buttonbox").css("opacity", 1);
+      context.drawImage(Video, 0, 0, canvas.width, canvas.height);
+      context2.drawImage(Video2, 0, 0, canvas2.width, canvas2.height);
+      var imageData = context.getImageData( 0, 0, canvas.width, canvas.height);
+      pixels = imageData.data;
+      numPixels = imageData.width * imageData.height;
+      var imageData2 = context2.getImageData( 0, 0, canvas2.width, canvas2.height);
+      pixels2 = imageData2.data;
+      numPixels2 = imageData2.width * imageData2.height;
+      for ( var i = 0; i < numPixels; i++ ) {
+      var r = pixels[ i * 4 ],
+          g = pixels[ i * 4 + 1 ],
+          b = pixels[ i * 4 + 2 ],
+          r2=pixels2[ i * 4 ],
+          g2=pixels2[ i * 4 + 1 ],
+          b2=pixels2[ i * 4 + 2],
+          NIR = r2+g2+b2,
+          NDVI = (NIR-r)/(NIR+r);
+          if (NDVI>0.75) {
+          pixels[ i * 4 ] = 0;
+          pixels[ i * 4 + 1 ] = 255*NDVI;
+          pixels[ i * 4 + 2 ] = 0;            
+        }else if (NDVI<=0.75 && NDVI>0.25) {
+          pixels[ i * 4 ] = 255*(1-NDVI);
+          pixels[ i * 4 + 1 ] = 255*NDVI;
+          pixels[ i * 4 + 2 ] = 0;
+        }else {
+          pixels[ i * 4 ] = 255*(1-NDVI);
+          pixels[ i * 4 + 1 ] = 0;
+          pixels[ i * 4 + 2 ] = 0;  
+        }
+
+      }context.putImageData(imageData, 0, 0 );
+      // var data = canvas.toDataURL();
+      // var TimeBefore = Date.now();
+      // var Today = new Date (TimeBefore);
+      // var name = Today.toLocaleDateString();
+      // var link =document.createElement('a');
+      // link.href= data;
+      // link.download=name + "Analysis_Photo"
+      // document.body.appendChild(link);
+      // link.click();
+      // document.body.removeChild(link); 
     }else{
       $("#CamOff").css("opacity", 1);  //Camera off
       $("#LookPhoto").css("opacity", 0);
@@ -168,26 +222,13 @@ $(document).ready(function() {
     }
   });
 
-  //Take and save photo
-  $("#Analysis").click(function () {
-    var data = canvas.toDataURL();
-    var TimeBefore = Date.now();
-    var Today = new Date (TimeBefore);
-    var name = Today.toLocaleDateString();
-    var link =document.createElement('a');
-    link.href= data;
-    link.download=name
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    console.log("HI"); 
-  });
-
   //Stop Camera
   $("#StopCamara").click(function () {
     if (Video.unavalable == true) {
       Video.srcObject.getTracks()[0].stop();
-      Video.unavalable = false;   // Camera on
+      Video.unavalable = false;
+      Video2.srcObject.getTracks()[0].stop();
+      Video2.unavalable = false;   // Camera on
     }else{
       alert("Camera Is Off");//Camera off
     }
